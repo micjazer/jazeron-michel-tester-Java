cffdba6690f0a7c89d14d7c5eadef22364da6789
michel jazeron <michel.jazeron@gmail.com>
2025-02-20 10:15:39 +0100

    Modifications apportées dans le fichier FareCalculatorService.java, ajout d'une méthode : calculateFare(Ticket ticket, boolean discount) et ajout d'une méthode : calculateFare(Ticket ticket).

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,6 +5,14 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
+    /**
+     * Calcule le tarif de stationnement pour un ticket donné.
+     *
+     * @param ticket Le ticket contenant les informations de stationnement, y compris les heures d'entrée et de sortie.
+     * @param discount Indique si une remise de 5% doit être appliquée au tarif calculé.
+     *
+     * @throws IllegalArgumentException si l'heure de sortie est nulle ou antérieure à l'heure d'entrée.
+     */
     public void calculateFare(Ticket ticket, boolean discount) {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
@@ -36,11 +44,16 @@ public class FareCalculatorService {
 
         double price = duration * rate;
         if (discount) {
-            price *= 0.95; // Apply 5% discount
+            price *= 0.95; // Applique 5% de remise
         }
         ticket.setPrice(price);
     }
 
+    /**
+     * Calcule le tarif de stationnement pour un ticket donné sans appliquer de remise.
+     *
+     * @param ticket Le ticket contenant les informations de stationnement, y compris les heures d'entrée et de sortie.
+     */
     // Méthode surchargée pour la compatibilité vers l'arrière
     public void calculateFare(Ticket ticket) {
         calculateFare(ticket, false);
